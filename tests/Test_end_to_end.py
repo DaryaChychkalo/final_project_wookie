aa72c0cf0ca76bba763ba46b25bcb818c6b29380
DaryaChychkalo <daryachychkalo@gmail.com>
2024-03-10 17:10:29 +0200

    Add files

--- tests/Test_end_to_end.py
+++ tests/Test_end_to_end.py
@@ -21,14 +21,12 @@ def test_buy_products(browser, login_data):
         page = browser.new_context().new_page()
         wookie_page = WookiePage(page)
         wookie_page.open_homepage()
-
         wookie_page.login(login_data['email'], login_data['password'])
         wookie_page.search_for_product("Бездротові навушники Xiaomi Buds 3")
         wookie_page.add_product_to_cart("Бездротові навушники Xiaomi Buds 3 (BHR5526GL) - White")
         wookie_page.go_to_catalog()
         wookie_page.select_category("Samsung - купити на Wookie.UA")
         wookie_page.select_category("Серія Samsung Galaxy A")
-
         wookie_page.select_product("Samsung Galaxy A73")
         wookie_page.add_to_cart_multi("Захисний чохол IMAK UC-2 Series для Samsung Galaxy A73 (A736) - Red: фото 1 з")
         wookie_page.go_to_catalog()
