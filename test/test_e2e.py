1c185ad6432b0180c10393924d05b9cd478f162a
DaryaChychkalo <daryachychkalo@gmail.com>
2024-03-10 17:27:50 +0200

    Add files

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import sync_playwright, Playwright, expect
+from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="module")
 def browser():
